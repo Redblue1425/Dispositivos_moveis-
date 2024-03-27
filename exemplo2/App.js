e0cd359c4ff44028f5d733a20bb6ccaac1f9ec92
Redblue1425 <luuucasantunes10@gmail.com>
2024-03-27 10:16:29 -1200

    alterações do dia 27/03

--- exemplo2/App.js
+++ exemplo2/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+/*import React from "react";
 import {View, Text} from "react-native";
 
 function App(){
@@ -9,4 +9,29 @@ function App(){
     );
 }
 
+export default App;*/
+
+import React, {Component} from'react';
+import {View, Text, Image} from'react-native';
+
+class App extends Component{
+    render(){
+        let curso = 'INFO';
+
+        return(
+            <View>
+                <Text>Olá Mundo!</Text>
+                <Text>Exemplo!</Text>
+                <Text style={{color: 'blue', fontSize: 30, margin: 15}}></Text>
+
+                <Image source={{uri: 'coloca a url aqui'}} style = {{width: 300, height: 300}}/>
+                <Text>{curso}</Text>
+            </View>
+        );
+    
+}
+}
+
 export default App;
+
+
